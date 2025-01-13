c932b6feaeb1205e7244b789a3a5765793e5e5dd
Georgecip1 <92641867+Georgecip1@users.noreply.github.com>
2025-01-13 11:18:11 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,20 +1,25 @@
 provider "google" {
-  project = "my-gcp-project"
-  region  = "us-central1"
+    project = "durable-height-447621-h7"
+    credentials = "${file("durable-height-447621-h7-dd1ddf2ecab7.json")}"
+    region = "us-central1"
+    zone = "us-central1-c"
 }
 
-resource "google_compute_instance" "example" {
-  name         = "vm-instance"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
+resource "google_compute_instance" "my_instance" {
+    name = "terraform-instance"
+    machine_type = "f1-micro"
+    zone =  "us-central1-a"
+    allow_stopping_for_update = true
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
+    boot_disk {
+      initialize_params {
+        image = "debian-cloud/debian-11"
+      }
     }
-  }
-
-  network_interface {
-    network = "default"
-  }
+    network_interface {
+      network = "default"
+      access_config {
+        //
+      }
+    }
 }
